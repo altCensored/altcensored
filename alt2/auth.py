de68adce70d9ee0f329c4d6fa9b2c0755d2c3ac7
Nathan <>
2020-08-03 14:44:28 -0700

    stubbed login session

--- alt2/auth.py
+++ alt2/auth.py
@@ -1,8 +1,19 @@
-from flask import (Blueprint, redirect)
+from flask import (Blueprint, redirect, request, current_app, session)
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['POST'])
 def login():
+    email = request.form['email']
+    password = request.form['password']
+    session['logged_in'] = True
+    return redirect('/')
+
+
+@bp.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
     return redirect('/')

ca2aa961fd4e1198e87dc0ddd85b16f5036fe5b7
Admin altCensored <admin@altcensored.com>
2020-08-12 17:43:29 +0000

    Revert "Merge branch 'lin-file-perms' into 'master'"
    This reverts merge request !12

--- alt2/__init__.py
+++ alt2/__init__.py
@@ -141,15 +141,13 @@ def create_app(test_config=None):
         db_session.remove()    
 
     # apply the blueprints to the app
-    from . import video, channel, about, category, language, settings, auth, subscription
+    from . import video, channel, about, category, language, settings
     app.register_blueprint(video.bp)
     app.register_blueprint(channel.bp)
     app.register_blueprint(about.bp)
     app.register_blueprint(category.bp)
     app.register_blueprint(language.bp)
     app.register_blueprint(settings.bp)
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(subscription.bp)
     # app.register_blueprint(auth.bp)
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with

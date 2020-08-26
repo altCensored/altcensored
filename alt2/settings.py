6dba8cb97e322cdfcb4ecb0165538fe1e808fca8
Admin altCensored.com <admin@altCensored.com>
2020-08-26 19:04:01 +0000

    settings: clean up .py

--- alt2/settings.py
+++ alt2/settings.py
@@ -15,32 +15,15 @@ bp = Blueprint('settings', __name__, url_prefix='/settings' )
 def index():
 
 	if request.method == 'POST':
-#		message='POST'
-#		flash(message)  
 		locale = request.form['locale']
 		session['locale'] = request.form['locale']
-#		g.locale = locale
-
-#	else:
-#		g.locale = request.accept_languages.best_match(current_app.config['SUPPORTED_LANGUAGES'].keys())
-#		locale = g.locale
-#		message='NOT POST'
-#		flash(message)  
-#		error = None
-
-#    channelcount = db_session.query(func.count(Mv_Channel.ytc_id)).scalar()
 	videocount = db_session.query(func.count(Mv_Video.extractor_data)).scalar()
 	delchannelcount = db_session.query(func.count(Mv_Channel.ytc_id)).filter(Mv_Channel.ytc_deleted).scalar()
-#    archchancount = db_session.query(func.count(Mv_Channel.ytc_id)).filter(Mv_Channel.ytc_archive).scalar()
-#    slanguages = ['en', 'pt', 'de']
-#    languages = ['en', 'pt', 'de']
 	languages = (current_app.config['SUPPORTED_LANGUAGES'].keys())
-#	if locale != None:
 	locale = session['locale']
 	languages_list=list(languages)
 	languages_list.remove(locale)
 	languages = languages_list
-
 
 	return render_template('settings/settings_index.html', videocount=videocount, \
 		delchannelcount=delchannelcount,languages=languages,locale=locale)

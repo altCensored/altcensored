62dbcba36e1be97ed4bceefe97a7c68470792842
Admin altCensored.com <admin@altCensored.com>
2020-09-07 20:24:09 +0000

    made check for duplicate user.username case insensitive

--- alt2/settings.py
+++ alt2/settings.py
@@ -16,7 +16,7 @@ bp = Blueprint('settings', __name__, url_prefix='/settings' )
 
 def find_username_by_username(username):
     try:
-        return db_session.query(User).filter(User.username == username).one()
+        return db_session.query(User).filter(func.lower(User.username) == func.lower(User.username)).one()
     except NoResultFound:
         return None
 
@@ -24,6 +24,18 @@ def username_exists(username):
     if username == session['user']['name']:
         return False
     return find_username_by_username(username) is not None
+
+
+def username_exists2(username):
+    if username == session['user']['name']:
+        return False
+#    if db_session.query(User.username).filter_by(username=username).scalar() is not None:
+    if db_session.query(User.username).filter(func.lower(User.username) == func.lower(username)).scalar() is not None:
+        return True
+
+#models.User.query.filter_by(username="GANYE").first()
+#models.User.query.filter(func.lower(User.username) == func.lower("GaNyE")).first()
+
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
@@ -85,7 +97,7 @@ def index():
                 flash('Profanity not allowed', 'error')
                 return redirect(url_for('settings.index'))
 
-            if username_exists(fusername):
+            if username_exists2(fusername):
                 flash('Username already exists', 'error')
                 return redirect(url_for('settings.index'))
  

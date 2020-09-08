3a6a962039c35d40b7cb0d1ee3663b6d81ad9171
Admin altCensored.com <admin@altCensored.com>
2020-09-08 15:53:29 +0000

    check user_public flag, drop use of db_session object

--- alt2/user.py
+++ alt2/user.py
@@ -4,7 +4,6 @@ from flask import (
 
 from sqlalchemy import func
 from .models import User
-from .database import db_session
 from .pagination import Pagination
 
 bp = Blueprint('user', __name__, url_prefix='/user' )
@@ -15,8 +14,9 @@ PER_PAGE = 24
 @bp.route('/page/<int:page>')
 def index(page):
     offset = ((int(page)-1) * PER_PAGE)
-    usercount = db_session.query(func.count(User.id)).scalar()
-    users = User.query.limit(PER_PAGE).offset(offset)
+    usercount = User.query.filter(User.user_public).count()
+    users = User.query.filter(User.user_public).limit(PER_PAGE).offset(offset)
+
     if not users and page != 1:
         abort(404)
     pagination = Pagination(page, PER_PAGE, usercount)
@@ -27,7 +27,8 @@ def index(page):
 
 @bp.route('/<user>')
 def item(user):
-    user = db_session.query(User).filter(func.lower(User.username) == func.lower(user)).scalar()
+    user = User.query.filter(func.lower(User.username) == func.lower(user)).scalar()
+
     if not user and page != 1:
         abort(404)
     return render_template('user/user_item.html', 

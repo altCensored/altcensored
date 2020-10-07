a08c659e5dbdf5aff4b8fa667196f95b1f999757
Admin altCensored <admin@altCensored.com>
2020-10-07 16:47:25 +0000

    - removed 'language' from header, added as navbar option - moved 'about' from header to footer - cleaned up footer, added 'privacy' and 'faq' - added 'privacy' 'faq' and 'jscript licenses' to about

---
+++ migrations/002_populate_translation.sql
@@ -0,0 +1,7 @@
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab1', 'video', 'video', 'vídeo', 'vidéo', 'vídeo', 'video', 'video', 'video');
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab2', 'channel', 'kanal', 'canal', 'canal', 'canal', 'kanaal', 'canale', 'kanal');
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab3', 'playlist', 'playlist', 'playlist', 'playlist', 'playlist', 'afspeellijst', 'playlist', 'spellista');
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab4', 'category', 'kategorie', 'categoría', 'catégorie', 'categoria', 'categorie', 'categoria', 'kategori');
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab5', 'settings', 'einstellungen', 'ajustes', 'paramètres', 'configurações', 'instellingen', 'impostazioni', 'inställningar');
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab6', 'user', 'benutzer', 'usuario', 'usager', 'usuário', 'gebruiker', 'utente', 'användare');
+INSERT INTO "translation"("varname", "en", "de", "es", "fr", "pt", "nl", "it", "se") VALUES ('navtab7', 'languages', 'sprachen', 'idiomas', 'langues', 'idiomas', 'sprog', 'lingue', 'språk');

56e1b3a459ea49def38bdb0832ab4071af9ca880
admin altCensored.com <admin@altCensored.com>
2020-11-04 01:03:10 -0300

    tweaking3

--- alt2/util.py
+++ alt2/util.py
@@ -124,8 +124,8 @@ def set_session() -> object:
         pass
     else:
 #        my_session = str(session['locale'])
-        row = db_session.query(Translation).with_entities(Translation.varname,getattr(Translation, 'en')).all()
-#        row = db_session.query(Translation).with_entities(Translation.varname,getattr(Translation, session['locale'])).all()
+#        row = db_session.query(Translation).with_entities(Translation.varname,getattr(Translation, 'en')).all()
+        row = db_session.query(Translation).with_entities(Translation.varname,getattr(Translation, session['locale'])).all()
         rowtuple = tuple(row)
         session['navtabs'] = dict(rowtuple)
 

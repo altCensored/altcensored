1b16ae7e6a6f16c568149e718447030c30f57860
admin altCensored.com <admin@altCensored.com>
2021-12-19 13:37:35 -0300

    changed IPROXY static values to 127.0.0.1

--- alt2/config.py
+++ alt2/config.py
@@ -9,10 +9,10 @@ MYSERVER_NAME="altCensored.com"
 MYSERVER_URL="https://altCensored.com"
 MYSERVER_TEST_URL="https://altCensored.com"
 IARCHIVEURL="https://archive.org/download/youtube-"
-IPROXY="https://altcensored.com/ip/180x100/"
-IPROXYBIG="https://altcensored.com/ip/320x180/"
-IPROXYTW="https://altcensored.com/ip/144x144/"
-IPROXYTWBIG="https://altcensored.com/ip/"
+IPROXY="https://127.0.0.1/ip/180x100/"
+IPROXYBIG="https://127.0.0.1/ip/320x180/"
+IPROXYTW="https://127.0.0.1/ip/144x144/"
+IPROXYTWBIG="https://127.0.0.1/ip/"
 
 SUPPORTED_LANGUAGES = {'en': 'English', 'de': 'Deutsch', 'es': 'Español', 'fr': 'Français', 'pt': 'Portuguese', 'nl': 'Nederlandse', 'it': 'Italiano', 'se': 'Sverige'}
 SUPPORTED_THEMES = {'light', 'dark'}

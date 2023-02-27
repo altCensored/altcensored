75376fb9dd9c565b1275aa5ad5a4b95294605b39
admin altCensored <admin@altcensored.com>
2023-02-27 18:54:01 -0300

    add example for embed at /about/example

--- alt2/about.py
+++ alt2/about.py
@@ -9,10 +9,11 @@ bp = Blueprint('about', __name__, url_prefix='/about' )
 
 @bp.route('/')
 def index():
-#    set_session()
     archchancount = db_session.query(func.count(Mv_Channel.ytc_id)).filter(Mv_Channel.ytc_archive).scalar()
     channels = Mv_Channel.query.all()
 
     return render_template('about/about_index.html', channels=channels, archchancount=archchancount)
 
-
+@bp.route('/example')
+def example():
+    return render_template('about/about_example.html')

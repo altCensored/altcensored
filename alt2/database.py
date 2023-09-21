08ce1719af349e2b65f963b1c2749f8656ff820a
altCensored.com <admin@altCensored.com>
2023-09-21 13:19:26 -0300

    tweaking pool size number

--- alt2/database.py
+++ alt2/database.py
@@ -5,8 +5,8 @@ from . import config
 dbase = config.SQLALCHEMY_DATABASE_URI
 
 engine = create_engine(dbase,
-                       pool_size=8,
-                       max_overflow=30,
+                       pool_size=5,
+                       max_overflow=25,
 #                       echo=True,
 #                       echo_pool="debug",
 #                       pool_use_lifo=True,

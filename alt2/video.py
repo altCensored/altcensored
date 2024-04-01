1af6a1b715e57e36a81ba47ff7a9b16d22e9be58
altCensored.com <admin@altCensored.com>
2024-04-01 20:14:49 -0300

    adding IA video file download, tweak

--- alt2/video.py
+++ alt2/video.py
@@ -187,10 +187,10 @@ def watch():
             rsps = item.download(video_files, verbose=True, destdir=IARCHIVEITEMFS, dry_run=False)
 
             if all([r.status_code == 200 for r in rsps]):
-#                old_file_full = (video_files[0])
-#                old_file_ext = (os.path.splitext(old_file_full)[1])
-#                new_file_full = video_id + old_file_ext
-#                os.rename(IARCHIVEITEMFS + video_id + "/" + old_file_full,IARCHIVEITEMFS + video_id + "/" + new_file_full)
+                old_file_full = (video_files[0])
+                old_file_ext = (os.path.splitext(old_file_full)[1])
+                new_file_full = video_id + old_file_ext
+                os.rename(IARCHIVEITEMFS + video_id + "/" + old_file_full,IARCHIVEITEMFS + video_id + "/" + new_file_full)
             else:
                 pass
             flash(Markup(' \

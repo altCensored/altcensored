60e5c1512b3ce1fbe59e2485b1675908f4fa8738
altCensored.com <admin@altCensored.com>
2024-07-27 20:50:50 -0300

    bumped to max 300

--- alt2/database.py
+++ alt2/database.py
@@ -6,7 +6,7 @@ dbase = config.SQLALCHEMY_DATABASE_URI
 
 engine = create_engine(dbase,
                        pool_size=7,
-                       max_overflow=100,
+                       max_overflow=20,
 #                       echo=True,
 #                       echo_pool="debug",
 #                       pool_use_lifo=True,

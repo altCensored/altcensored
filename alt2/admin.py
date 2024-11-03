d9a7b79248c47cde681394862a80a038d2146c5b
altCensored.com <admin@altCensored.com>
2024-11-03 10:46:22 -0300

    - added -ur flag to admin.py mirror_channel command to force resync, tweaked

--- alt2/admin.py
+++ alt2/admin.py
@@ -337,13 +337,14 @@ def mirror_channel():
         sys_name = s3_server
         channel_id = (request.form['channel_id'])
         channel_url = "https://www.youtube.com/playlist?list=UU" + (channel_id[2:])
-        action = ' mirror -ur '
-        cookie = ' -cf $COOKIE_FILE'
+        action = ' mirror '
+        cookie = ' -cf $COOKIE_FILE '
+        resync = ' -ur '
 
         params1 = ' nohup yt-syncac '
         params2 = ' -f > /root/nohup_ssh.out 2>&1 &'
 
-        command = params1 + action + channel_url + cookie + params2
+        command = params1 + action + channel_url + cookie + resync + params2
         commands = [command]
         ssh_command(sys_name, commands)
 

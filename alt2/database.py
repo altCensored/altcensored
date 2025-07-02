1c500c22138b35e04c8eef6ed4ae23873df519f2
altcensored <admin@altcensored.com>
2025-07-02 06:22:49 -0300

    bumped up sqlalchemy pool size and overflow from 5/25 to 6/30

--- alt2/database.py
+++ alt2/database.py
@@ -5,8 +5,10 @@ from . import config
 dbase = config.SQLALCHEMY_DATABASE_URI
 
 engine = create_engine(dbase,
-                       pool_size=5,
-                       max_overflow=25,
+#                       pool_size=5,
+#                       max_overflow=25,
+                       pool_size=6,
+                       max_overflow=30,
 #                       echo=True,
 #                       echo_pool="debug",
 #                       pool_use_lifo=True,
